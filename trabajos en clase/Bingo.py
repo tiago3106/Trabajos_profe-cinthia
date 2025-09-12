06c3aafdfa1f5b6c3219b1b448ee8d200cfa2988
tiago3106 <tiagogd3101@gmail.com>
2025-09-11 23:30:43 -0300

    tarea lunes
    no lo pude subir antes que me quede bloqueado con este ejercicio

--- trabajos en clase/Bingo.py
+++ trabajos en clase/Bingo.py
@@ -1,10 +1,24 @@
 #bingo 5 x 5 
 import random
-ganar = False
-filas = 5   
+ganar = 0
+filas = 5
+columna =5   
 num = random.sample(range(1,51),25)
 talon = [random.sample(num[i:i+5],5) for i in range(0,25,5)]
 print("Bienvenido al Bingo")
 print("Tu talon es: ")
 for fila in talon:
     print(fila)
+while ganar == 0:
+    num_sorteados = random.sample(range(1,51),50)
+    for numero in num_sorteados:
+        print(f"El numero que salio es: {numero}")
+        for i in range(filas):
+            for j in range(columna):
+                if talon[i][j] == numero:
+                    print(f"¡Número {numero} marcado en tu cartón!")
+                    talon[i][j] = 0
+                    print(talon,"\n")
+    if all(all(x == 0 for x in fila) for fila in talon):
+        print("¡Bingo en el cartón!")
+        ganar = 1
